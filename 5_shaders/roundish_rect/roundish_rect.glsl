c0be7d98686610e00d0a5fbdb2f7e5e5f6c1f86d
kivattt <kivattt@gmail.com>
2025-04-09 00:58:47 +0200

    Restructure 5_shaders

--- 5_shaders/roundish_rect/roundish_rect.glsl
+++ 5_shaders/roundish_rect/roundish_rect.glsl
@@ -1,8 +1,8 @@
 #version 330
 
 uniform ivec4 box;
-uniform vec4 color = vec4(1, 1, 1, 1);
 uniform int screen_height;
+uniform vec4 color = vec4(1, 1, 1, 1);
 
 void main() {
 	gl_FragColor = color;
